79d29bc5a754211196733fdd7abf5f5db675eb6d
rafy <rafy@evtechnologies.ca>
2015-09-14 10:15:04 -0700

    Initial Commit

---
+++ RotaryWheel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RotaryWheel
+//
+//  Created by zhaofei on 2015-09-14.
+//  Copyright © 2015 zhaofei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
